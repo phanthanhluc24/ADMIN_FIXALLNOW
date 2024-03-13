827615aae4ea6232e9e4d465fcda903619a5e103
Phan Thanh Luc <luc.phan24@student.passerellesnumeriques.org>
2024-03-13 14:28:42 +0700

    style:(FAN-227): Design Manage Admin Page

---
+++ src/components/home/RepairmanFinder.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const RepairmanFinder = () => {
+  return (
+    <div className="container_repairman">
+    <div className="table-repairman">
+      <table className='table'>
+        <tr>
+          <th className='tr-1'>STT</th>
+          <th className='tr-2'>Họ và tên</th>
+          <th className='tr-3'>Số lần đặt</th>
+          <th className='tr-4'>Số lần hủy</th>
+          <th className='tr-5'>Hành động</th>
+        </tr>
+        <tr>
+          <td>
+
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+  )
+}
---
+++ src/components/home/Service.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Service = () => {
+  return (
+    <div className="container_repairman">
+    <div className="table-repairman">
+      <table className='table'>
+        <tr>
+          <th className='tr-1'>STT</th>
+          <th className='tr-6'>Thợ</th>
+          <th className='tr-7'>Dịch vụ</th>
+          <th className='tr-8'>Giá</th>
+          <th className='tr-9'>Ảnh bìa</th>
+          <th className='tr-10'>Mô tả</th>
+          <th className='tr-11'>Hành động</th>
+        </tr>
+        <tr>
+          <td>
+
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+  )
+}
